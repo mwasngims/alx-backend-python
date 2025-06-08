151b0aafa97c744e4bb8c057b2fdf381f6cfa242
mwasngims <mwangiwachira700@gmail.com>
2025-06-08 18:54:49 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
@@ -6,7 +6,7 @@ from rest_framework.decorators import action
 from .models import User, Conversation, Message
 from .permissions import IsParticipantOfConversation
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.exceptions import PermissionDenied
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """
@@ -23,18 +23,11 @@ class ConversationViewSet(viewsets.ModelViewSet):
         return Conversation.objects.filter(participants=self.request.user)
 
     def list(self, request, *args, **kwargs):
-        """
-        List all conversations with their participants and messages.
-        """
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
-        """
-        Create a new conversation with participants.
-        Expects a list of user IDs in 'participants'.
-        """
         participants = request.data.get('participants', [])
         if not participants or not isinstance(participants, list):
             return Response({'error': 'Participants must be a list of user IDs.'}, status=status.HTTP_400_BAD_REQUEST)
@@ -42,8 +35,6 @@ class ConversationViewSet(viewsets.ModelViewSet):
         conversation.participants.set(participants)
         serializer = self.get_serializer(conversation)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
 
 class MessageViewSet(viewsets.ModelViewSet):
     """
@@ -57,21 +48,33 @@ class MessageViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
+        conversation_id = self.request.query_params.get('conversation_id')
+        if conversation_id:
+            conversation = get_object_or_404(Conversation, id=conversation_id)
+            if self.request.user not in conversation.participants.all():
+                # User is not participant, forbid access
+                from rest_framework.status import HTTP_403_FORBIDDEN
+                raise PermissionDenied(detail="You do not have permission to view messages for this conversation.", code=HTTP_403_FORBIDDEN)
+            return Message.objects.filter(conversation=conversation)
+        # If no conversation_id specified, return all messages user can see
         return Message.objects.filter(conversation__participants=self.request.user)
 
     def list(self, request, *args, **kwargs):
-        """
-        List all messages with their sender and conversation details.
-        """
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
-        """
-        Send a message to an existing conversation.
-        Expects 'conversation', 'sender', and 'message_body' in the request data.
-        """
+        conversation_id = request.data.get('conversation')
+        if not conversation_id:
+            return Response({'error': 'Conversation ID is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        conversation = get_object_or_404(Conversation, id=conversation_id)
+
+        if request.user not in conversation.participants.all():
+            from rest_framework.status import HTTP_403_FORBIDDEN
+            return Response({'detail': 'You do not have permission to send messages to this conversation.'}, status=HTTP_403_FORBIDDEN)
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
